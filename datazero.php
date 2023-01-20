f314c29abbd8fdad8d0917ea90c1072a0f89c5b6
aldosena <36321317+aldosena@users.noreply.github.com>
2023-01-20 13:35:53 -0300

    Update datazero.php

--- datazero.php
+++ datazero.php
@@ -7,11 +7,16 @@
 # 02-12-2021 = a funcao diaextenso( recebe uma melhoria para aceitar barra ou traço	 
 # 21-09-2021 = criada a funçao datafinal(....
 # 24-09-2020 = retorno "" se não consegir converter para BR 
-# 24-09-2020 = o datacertadma agora aceita / ou -
 # Objetivo: Funçoes com datas (PHP7)
 
+// menusculo em desuso
+$CODIGODODIA = array("DOM" => "1", "SEG" => "2", "TER" => "3", "QUA" => "4", "QUI" => "5", "SEX" => "6", "SAB" => "7");
 $codigododia = array("DOM" => "1", "SEG" => "2", "TER" => "3", "QUA" => "4", "QUI" => "5", "SEX" => "6", "SAB" => "7");
+
+$AMESES = array(1 => "Janeiro", 2 => "Fevereiro", 3 => "Março", 4 => "Abril", 5 => "Maio", 6 => "Junho", 7 => "Julho", 8 => "Agosto", 9 => "Setembro", 10 => "Outubro", 11 => "Novembro", 12 => "Dezembro");
 $ameses = array(1 => "Janeiro", 2 => "Fevereiro", 3 => "Março", 4 => "Abril", 5 => "Maio", 6 => "Junho", 7 => "Julho", 8 => "Agosto", 9 => "Setembro", 10 => "Outubro", 11 => "Novembro", 12 => "Dezembro");
+
+$AMS = array(1 => "Jan", 2 => "Fev", 3 => "Mar", 4 => "Abr", 5 => "Mar", 6 => "Jun", 7 => "Jul", 8 => "Ago", 9 => "Set", 10 => "Out", 11 => "Nov", 12 => "Dez");
 $ams = array(1 => "Jan", 2 => "Fev", 3 => "Mar", 4 => "Abr", 5 => "Mar", 6 => "Jun", 7 => "Jul", 8 => "Ago", 9 => "Set", 10 => "Out", 11 => "Nov", 12 => "Dez");
 
 function datafinal($dx, $xsaida){   
